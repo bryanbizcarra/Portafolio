0b1e3932dd49a8cbdc8ed72d716d661935cd4d19
Bryan <b.bizcarra@duocuc.cl>
2021-08-12 12:55:25 -0400

    Corrección bug menu responsive

--- estaticos/js/scripts.js
+++ estaticos/js/scripts.js
@@ -1,3 +1,7 @@
 botonMenu = document.getElementById('boton-menu')
 menu = document.getElementById('menu')
+listaMenu = document.querySelectorAll('#opciones')
 botonMenu.addEventListener('click', ()=>menu.classList.toggle('menu'))
+for (let i = 0; i < listaMenu.length; i++) {
+    listaMenu[i].addEventListener('click',()=>menu.classList.toggle('menu'))
+}
